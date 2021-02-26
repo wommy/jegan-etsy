0da64281a7148f1dc1aca4a5d896b5e02c2abf10
wommy <tom@technomad.media>
2021-02-26 13:30:50 -0500

    added new products, added link

--- 11ty/_data/products.js
+++ 11ty/_data/products.js
@@ -7,412 +7,527 @@ module.exports = [
 			"https://i.etsystatic.com/17972346/r/il/1ef787/2342551019/il_794xN.2342551019_mk65.jpg",
 			"https://i.etsystatic.com/17972346/r/il/cef610/2342551115/il_794xN.2342551115_d76d.jpg",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/730748851/art-lighter?ref=shop_home_active_66&frs=1&crt=1",
+		price: 10,
 	},
 	{
 		name: "skull lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/9ea0c8/2249086090/il_340x270.2249086090_44az.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/716893664/skull-lighter?ref=shop_home_active_65&frs=1",
+		price: 10,
 	},
 	{
 		name: "skull butterfly art lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/e2ccee/2103492181/il_340x270.2103492181_64mi.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/733189588/skull-butterfly-art-lighter?ref=shop_home_active_64&frs=1",
+		price: 10,
 	},
 	{
 		name: "holographic cassette tape sticker",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/3f6590/2086154350/il_340x270.2086154350_kp87.jpg?version=0",
 		],
-		price: 6
+		link: "https://www.etsy.com/listing/753592761/holographic-cassette-tape-sticker?ref=shop_home_active_63&frs=1",
+		price: 6,
 	},
 	{
 		name: "skull butterfly sticker",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/b43358/1993753936/il_340x270.1993753936_s3x8.jpg?version=0",
 		],
-		price: 5
+		link: "https://www.etsy.com/listing/690055425/skull-butterfly-sticker?ref=shop_home_active_62&frs=1",
+		price: 5,
 	},
 	{
 		name: "saunderson sisters hocus pocus sticker",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/4778e4/2006121010/il_340x270.2006121010_3bai.jpg?version=0",
 		],
-		price: 4
+		link: "https://www.etsy.com/listing/722179780/sanderson-sisters-hocus-pocus-sticker?ref=shop_home_active_61&frs=1",
+		price: 4,
 	},
 	{
 		name: "devil sticker",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/cac378/2198708285/il_340x270.2198708285_h3tj.jpg?version=0",
 		],
-		price: 5
+		link: "https://www.etsy.com/listing/756170332/devil-sticker?ref=shop_home_active_60&frs=1",
+		price: 5,
 	},
 	{
 		name: "aquarius zodiac lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/6d0949/2249113718/il_340x270.2249113718_bvs7.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/778229432/aquarius-zodiac-lighter?ref=shop_home_active_59&frs=1&crt=1",
+		price: 10,
 	},
 	{
 		name: "aries zodiac lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/fd2464/2296715607/il_340x270.2296715607_fgya.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/792112821/aries-zodiac-lighter?ref=shop_home_active_58&frs=1",
+		price: 10,
 	},
 	{
 		name: "cancer zodiac lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/5c8ecf/2249115912/il_340x270.2249115912_mrt4.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/778229642/cancer-zodiac-lighter?ref=shop_home_active_57&frs=1",
+		price: 10,
 	},
 	{
 		name: "capricorn zodiac lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/0e9858/2249116442/il_340x270.2249116442_mdcq.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/792113041/capricorn-zodiac-lighter?ref=shop_home_active_56&frs=1",
+		price: 10,
 	},
 	{
 		name: "gemini zodiac lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/a9969e/2249117026/il_340x270.2249117026_j0l7.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/792113195/gemini-zodiac-lighter?ref=shop_home_active_55&frs=1&crt=1",
+		price: 10,
 	},
 	{
 		name: "leo zodiac lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/4de498/2296717687/il_340x270.2296717687_ctyp.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/792113319/leo-zodiac-lighter?ref=shop_home_active_54&frs=1",
+		price: 10,
 	},
 	{
 		name: "libra zodiac lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/546ae1/2296718257/il_340x270.2296718257_rpwq.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/792113407/libra-zodiac-lighter?ref=shop_home_active_53&frs=1&crt=1",
+		price: 10,
 	},
 	{
 		name: "pisces zodiac lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/13aade/2296718751/il_340x270.2296718751_hpzb.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/778230174/pisces-zodiac-lighter?ref=shop_home_active_52&frs=1&crt=1",
+		price: 10,
 	},
 	{
 		name: "sagittarius zodiac lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/39f94c/2296719321/il_340x270.2296719321_k8cl.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/792113701/sagittarius-zodiac-lighter?ref=shop_home_active_51&frs=1&crt=1",
+		price: 10,
 	},
 	{
 		name: "scorpio zodiac lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/689219/2296720031/il_340x270.2296720031_nck1.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/792113827/scorpio-zodiac-lighter?ref=shop_home_active_50&frs=1&crt=1",
+		price: 10,
 	},
 	{
 		name: "taurus zodiac lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/cd0350/2337799263/il_340x270.2337799263_c7yd.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/778230624/taurus-zodiac-lighter?ref=shop_home_active_49&frs=1",
+		price: 10,
 	},
 	{
 		name: "virgo zodiac lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/b6d568/2296721013/il_340x270.2296721013_fer6.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/792114045/virgo-zodiac-lighter?ref=shop_home_active_48&frs=1&crt=1",
+		price: 10,
 	},
 	{
 		name: "show me your kitties lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/d74d49/2296703829/il_340x270.2296703829_1ufj.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/716894038/show-me-your-kitties-lighter?ref=shop_home_active_47&frs=1",
+		price: 10,
 	},
 	{
 		name: "devil black lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/6d3a76/2249105898/il_340x270.2249105898_nfu6.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/716894210/devil-lighter?ref=shop_home_active_46&frs=1",
+		price: 10,
 	},
 	{
 		name: "cute art lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/8016cf/2296686141/il_340x270.2296686141_9z7g.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/730748595/cute-art-lighter?ref=shop_home_active_45&frs=1&crt=1",
+		price: 10,
 	},
 	{
 		name: "butterfly art lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/26a443/2103492847/il_340x270.2103492847_be6k.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/733189752/butterfly-art-lighter?ref=shop_home_active_44&frs=1",
+		price: 10,
 	},
 	{
 		name: "casette tape lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/d2a8c9/2055903396/il_340x270.2055903396_60zb.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/747052227/casette-tape-lighter?ref=shop_home_active_43&frs=1",
+		price: 10,
 	},
 	{
 		name: "skull flame lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/d0ffe0/2249122932/il_340x270.2249122932_c3w9.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/778231224/skull-flame-lighter?ref=shop_home_active_42&frs=1",
+		price: 10,
 	},
 	{
 		name: "skull cone lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/142a74/2055904310/il_340x270.2055904310_2ey3.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/747052369/skull-cone-lighter?ref=shop_home_active_41&frs=1&crt=1",
+		price: 10,
 	},
 	{
 		name: "spooky smoke lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/5e5f61/2103491405/il_340x270.2103491405_jz2e.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/733189468/spooky-smoke-art-lighter?ref=shop_home_active_40&frs=1",
+		price: 10,
 	},
 	{
 		name: "devil white lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/8e256b/2249121508/il_340x270.2249121508_qbvt.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/792114223/devil-lighter?ref=shop_home_active_39&frs=1",
+		price: 10,
 	},
 	{
 		name: "grim reaper lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/588e18/2296722439/il_340x270.2296722439_cqmr.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/778231112/grim-reaper-lighter?ref=shop_home_active_38&frs=1",
+		price: 10,
 	},
 	{
 		name: "skull line art lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/0450a8/2103485141/il_340x270.2103485141_cb6w.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/747051251/skull-line-art-lighter?ref=shop_home_active_37&frs=1&crt=1",
+		price: 10,
 	},
 	{
 		name: "fishnet art lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/c671ff/2055901938/il_340x270.2055901938_89y8.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/733188752/fishnet-art-lighter?ref=shop_home_active_36&frs=1&crt=1",
+		price: 10,
 	},
 	{
 		name: "bondage art lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/6655dc/2055899972/il_340x270.2055899972_57z3.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/733188496/bondage-art-lighter?ref=shop_home_active_35&frs=1",
+		price: 10,
 	},
 	{
 		name: "nun art lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/e64ec6/2103488795/il_340x270.2103488795_3uv2.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/747052055/nun-art-lighter?ref=shop_home_active_34&frs=1",
+		price: 10,
 	},
 	{
 		name: "butterfly skull patch",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/62c503/2224366971/il_340x270.2224366971_3d3x.jpg?version=0",
 		],
-		price: 8
+		link: "https://www.etsy.com/listing/761941104/butterfly-skull-patch?ref=shop_home_active_33&frs=1",
+		price: 8,
 	},
 	{
 		name: "black phillip sticker",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/5aad7a/2342598809/il_340x270.2342598809_slzc.jpg?version=0",
 		],
-		price: 5
+		link: "https://www.etsy.com/listing/804360947/black-phillip-sticker?ref=shop_home_active_32&frs=1",
+		price: 5,
 	},
 	{
 		name: "lit lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/34a4a5/2296699231/il_340x270.2296699231_2tx1.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/730749071/lit-lighter?ref=shop_home_active_31&frs=1&crt=1",
+		price: 10,
 	},
 	{
 		name: "flower art lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/2bcfd3/2249085422/il_340x270.2249085422_pdte.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/716893798/flower-art-lighter?ref=shop_home_active_30&frs=1&crt=1",
+		price: 10,
 	},
 	{
 		name: "art lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/ac88c8/2055894422/il_340x270.2055894422_mdwg.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/747050847/art-lighter?ref=shop_home_active_29&frs=1",
+		price: 10,
 	},
 	{
 		name: "custom pet lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/636f36/2336363517/il_340x270.2336363517_kc3l.jpg?version=0",
 		],
-		price: 15
+		link: "https://www.etsy.com/listing/802606393/custom-pet-lighter?ref=shop_home_active_28&frs=1",
+		price: 15,
 	},
 	{
 		name: "butterfly skull pin",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/8b8f70/2176808394/il_340x270.2176808394_5oum.jpg?version=0",
 		],
-		price: 6
+		link: "https://www.etsy.com/listing/775816485/butterfly-skull-pin?ref=shop_home_active_27&frs=1",
+		price: 6,
 	},
 	{
 		name: "minimal line tattoo art",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/aea133/2406848316/il_340x270.2406848316_3ghg.jpg?version=0",
 		],
-		price: 25
+		link: "https://www.etsy.com/listing/837341849/minimal-line-tattoo-art?ref=shop_home_active_26",
+		price: 25,
 	},
 	{
 		name: "mushroom lady keychain",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/b3cf2e/2681341309/il_340x270.2681341309_r203.jpg?version=0",
 		],
-		price: 12
+		link: "https://www.etsy.com/listing/897821347/mushroom-lady-keychain?ref=shop_home_active_25&frs=1",
+		price: 12,
 	},
 	{
 		name: "custom pet line art book page print",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/ee90df/2690947036/il_340x270.2690947036_1atq.jpg?version=0",
 		],
-		price: 28
+		link: "https://www.etsy.com/listing/898285186/custom-pet-line-art-book-page-print?ref=shop_home_active_24&frs=1&sca=1",
+		price: 28,
 	},
 	{
 		name: "skull plant lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/d6e0db/2644177995/il_340x270.2644177995_n3qg.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/888515481/skull-plant-lighter?ref=shop_home_active_23&frs=1&crt=1",
+		price: 10,
 	},
 	{
 		name: "art lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/955edb/2249100302/il_340x270.2249100302_n6fz.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/690051873/art-lighter?ref=shop_home_active_22&frs=1&bes=1",
+		price: 10,
 	},
 	{
 		name: "plant lady lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/d4b9a6/2313243493/il_340x270.2313243493_g983.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/796204921/plant-lady-lighter?ref=shop_home_active_21&frs=1&crt=1",
+		price: 10,
 	},
 	{
 		name: "cute lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/bcc890/2249085104/il_340x270.2249085104_6l8t.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/730747983/cute-lighter?ref=shop_home_active_20&frs=1&crt=1",
+		price: 10,
 	},
 	{
 		name: "mushroom lady sticker",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/8d3f27/2436990116/il_340x270.2436990116_pg39.jpg?version=0",
 		],
-		price: 5
+		link: "https://www.etsy.com/listing/845922041/mushroom-lady-sticker?ref=shop_home_active_19&frs=1&crt=1",
+		price: 5,
 	},
 	{
 		name: "mushroom lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/c/1054/837/0/274/il/8f2fcf/2484628591/il_340x270.2484628591_7rhq.jpg",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/845923885/mushroom-lighter?ref=shop_home_active_18&frs=1&crt=1",
+		price: 10,
 	},
 	{
 		name: "2 pack art lighters",
 		imgs: [
 			"https://i.etsystatic.com/17972346/c/1042/828/0/57/il/707041/2187001361/il_340x270.2187001361_edk7.jpg",
 		],
-		price: 18
+		link: "https://www.etsy.com/listing/715350422/2-pack-art-lighters-choose-your-designs?ref=shop_home_active_17&frs=1&crt=1",
+		price: 18,
 	},
 	{
 		name: "art candle",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/00b34c/2583905519/il_340x270.2583905519_byrs.jpg?version=0",
 		],
-		price: 35
+		link: "https://www.etsy.com/listing/873065291/art-candle?ref=shop_home_active_16&frs=1",
+		price: 35,
 	},
 	{
 		name: "skull plant sticker",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/d3e952/2596506030/il_340x270.2596506030_t01q.jpg?version=0",
 		],
-		price: 5
+		link: "https://www.etsy.com/listing/888516507/skull-plant-sticker?ref=shop_home_active_15&frs=1",
+		price: 5,
 	},
 	{
 		name: "mushroom art candle",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/4f383c/2677634718/il_340x270.2677634718_638e.jpg?version=0",
 		],
-		price: 35
+		link: "https://www.etsy.com/listing/908845997/mushroom-art-candle?ref=shop_home_active_14&frs=1&crt=1",
+		price: 35,
 	},
 	{
 		name: "custom nude art lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/c/2203/1747/0/303/il/b035ac/2832303698/il_340x270.2832303698_h4c9.jpg",
 		],
-		price: 15
+		link: "https://www.etsy.com/listing/952531041/custom-nude-art-lighter?ref=shop_home_active_13&frs=1&crt=1",
+		price: 15,
 	},
 	{
 		name: "zodiac birthday lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/99a960/2927706757/il_340x270.2927706757_mcbc.jpg?version=0",
 		],
-		price: 10
+		link: "https://www.etsy.com/listing/964882929/zodiac-birthday-lighter?ref=shop_home_active_12&frs=1&crt=1",
+		price: 10,
 	},
 	{
 		name: "custom bachelor/bachelorette lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/c/2203/1747/0/303/il/b035ac/2832303698/il_340x270.2832303698_h4c9.jpg",
 		],
-		price: 15
+		link: "https://www.etsy.com/listing/950923990/custom-bachelorbachelorette-lighter?ref=shop_home_active_11&frs=1",
+		price: 15,
 	},
 	{
 		name: "custom wedding lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/c/2250/1784/0/766/il/a42c16/2933174801/il_340x270.2933174801_1gel.jpg",
 		],
-		price: 15
+		link: "https://www.etsy.com/listing/966264005/custom-wedding-lighter?ref=shop_home_active_10&frs=1",
+		price: 15,
 	},
 	{
 		name: "lesbian wedding ring lighter",
 		imgs: [
 			"https://i.etsystatic.com/17972346/c/2250/1784/0/804/il/ea4b73/2885505382/il_340x270.2885505382_kysl.jpg",
 		],
-		price: 15
+		link: "https://www.etsy.com/listing/952301202/lesbian-wedding-ring-lighter?ref=shop_home_active_9&frs=1",
+		price: 15,
 	},
 	{
 		name: "custom anniversary lighter gift",
 		imgs: [
 			"https://i.etsystatic.com/17972346/d/il/9d6c47/2933180915/il_340x270.2933180915_th23.jpg?version=0",
 		],
-		price: 15
+		link: "https://www.etsy.com/listing/966265859/custom-anniversary-lighter-gift?ref=shop_home_active_8&frs=1&sca=1",
+		price: 15,
+	},
+	{
+		name: "cute lighter skin (wrap only)",
+		imgs: [
+			"https://i.etsystatic.com/17972346/d/il/98132f/2942525989/il_340x270.2942525989_i497.jpg?version=0",
+		],
+		link: "https://www.etsy.com/listing/954627072/cute-lighter-skin-wrap-only?ref=shop_home_active_7&frs=1",
+		price: 5,
+	},
+	{
+		name: "mushroom lighter skin (wrap only)",
+		imgs: [
+			"https://i.etsystatic.com/17972346/d/il/4d96d5/2894886624/il_680x540.2894886624_ajye.jpg?version=0",
+		],
+		link: "https://www.etsy.com/listing/954629214/mushroom-lighter-skin-wrap-only?ref=shop_home_active_6&frs=1",
+		price: 5,
+	},
+	{
+		name: "3 mushroom lighters + sticker set",
+		imgs: [
+			"https://i.etsystatic.com/17972346/c/2250/1784/0/591/il/d01e3d/2942576087/il_340x270.2942576087_7fha.jpg",
+		],
+		link: "https://www.etsy.com/listing/954631934/3-mushroom-lighters-sticker-set?ref=shop_home_active_5&frs=1",
+		price: 25,
+	},
+	{
+		name: "2 pack of cute lighter skins (wraps only)",
+		imgs: [
+			"https://i.etsystatic.com/17972346/d/il/98132f/2942525989/il_340x270.2942525989_i497.jpg?version=0",
+		],
+		link: "https://www.etsy.com/listing/968600825/2-pack-of-cute-lighter-skins-wraps-only?ref=shop_home_active_4&frs=1",
+		price: 10,
+	},
+	{
+		name: "2 pack of mushroom lighter skins (wraps only)",
+		imgs: [
+			"https://i.etsystatic.com/17972346/d/il/4d96d5/2894886624/il_340x270.2894886624_ajye.jpg?version=0",
+		],
+		link: "https://www.etsy.com/listing/968601421/2-pack-of-mushroom-lighter-skins-wraps?ref=shop_home_active_3&frs=1",
+		price: 5,
+	},
+	{
+		name: "2 art lighter skins (wraps only)",
+		imgs: [
+			"https://i.etsystatic.com/17972346/c/1042/828/0/57/il/707041/2187001361/il_680x540.2187001361_edk7.jpg",
+		],
+		link: "https://www.etsy.com/listing/968603161/2-art-lighter-skins-wraps-only?ref=shop_home_active_2&frs=1",
+		price: 10,
+	},
+	{
+		name: "custom protrait art lighter",
+		imgs: [
+			"https://i.etsystatic.com/17972346/d/il/2bef36/2942894441/il_340x270.2942894441_9zyw.jpg?version=0",
+		],
+		link: "https://www.etsy.com/listing/968680255/custom-portrait-art-lighter?ref=shop_home_active_1&frs=1",
+		price: 15,
 	},
 ]
